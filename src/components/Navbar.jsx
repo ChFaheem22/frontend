377e14bc4ab50e3b6fd75b7635fad7193ec4467b
ChFaheem22 <faheemchh779@gmail.com>
2025-07-11 17:06:05 +0500

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
               </>
             ) : (
               <>
-                <li><Link to="/login" className="btn btn-outline">Login</Link></li>
+                <li><Link to="/login" className="btn btn-outline2">Login</Link></li>
                 <li><Link to="/register" className="btn btn-primary">Register</Link></li>
               </>
             )}
